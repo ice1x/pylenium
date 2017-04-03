79ccae025cebd1baa3ca1486cd417bf031c9fdaf
ice1x <ice2600x@gmail.com>
2017-04-03 15:47:03 +0300

    Add non-selenium functions

--- pywebtest_lib.py
+++ pywebtest_lib.py
@@ -1,4 +1,13 @@
 from selenium import webdriver
+import urllib2
+import zlib
+from HTMLParser import HTMLParser
+import logging
+from logging import info
+import time
+
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
 class FirefoxDriver(webdriver.Firefox):
@@ -105,3 +114,65 @@ class FirefoxDriver(webdriver.Firefox):
         self.get(uri)
         self.find_element_by_xpath(_xpath).click()
         return self.current_url()
+
+
+class UrlFinder(HTMLParser):
+    def __init__(self, my_tag):
+        HTMLParser.__init__(self)
+        self.links = []
+        self.my_tag = my_tag
+
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        if self.my_tag == tag:
+            try:
+                self.links.append(attrs['href'])
+            except:
+                pass
+
+def urlopen(uri):
+    """
+    Get HTTP code
+    """
+
+    def try_urlopen(message='OK'):
+        try:
+            urllib2.urlopen(uri, timeout=30)
+        except urllib2.URLError, e:
+            if hasattr(e, 'code'):
+                message = str(e.code)
+            elif hasattr(e, 'reason'):
+                message = e.reason
+            info("%s - %s" % (message, str(uri)))
+            return message
+        except Exception, e:
+            info('Exception: %s - %s' % (str(uri), repr(e)))
+            time.sleep(1)
+            try_urlopen('OK')
+        return message
+
+    message = try_urlopen()
+    return message
+
+
+def parser(node, tag):
+    """
+    Parse web page "where - node" / "search inside this tag"
+    """
+    _parser = UrlFinder(tag)
+    try:
+        response = urllib.urlopen(node).read()
+    except Exception as e:
+        info("urllib failed: %s" % e.__repr__())
+        response = []
+
+    if response and len(response) > 0:
+        # NOTE: it was obsolete urllib.urlopen(node).headers.getheader('Content-Encoding') == 'gzip':
+        if urllib2.urlopen(node).headers.getheader('Content-Encoding') == 'gzip':
+            content = zlib.decompress(response, zlib.MAX_WBITS | 32)
+        else:
+            content = response
+        _parser.feed(content)
+        return _parser.links
+    else:
+        info('Content length: %s on %s by tag: %s' % (str(len(response)), str(node), str(tag)))
